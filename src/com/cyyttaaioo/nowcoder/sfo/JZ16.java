be88a26b461174b3c6907b0f5c00d542a6d7cb2c
cyyttaaioo <fxyclown@foxmail.com>
2021-07-29 16:25:59 +0800

    JZ16 合并两个排序的链表(添加了缺少的括号)

--- src/com/cyyttaaioo/nowcoder/sfo/JZ16.java
+++ src/com/cyyttaaioo/nowcoder/sfo/JZ16.java
@@ -8,24 +8,26 @@ public class JZ16 {
         ListNode(int val) {
             this.val = val;
         }
-}
-    public ListNode Merge(ListNode list1,ListNode list2) {
+    }
+
+    public ListNode Merge(ListNode list1, ListNode list2) {
         ListNode h = new ListNode(-1);
         ListNode n = h;
 
-        while(list1 !=null && list2 != null){
-            if(list1.val<=list2.val){
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
                 n.next = list1;
                 list1 = list1.next;
-            }else{
+            } else {
                 n.next = list2;
                 list2 = list2.next;
             }
             n = n.next;
         }
 
-        if(list1!=null) n.next = list1;
-        if(list2!=null) n.next = list2;
+        if (list1 != null) n.next = list1;
+        if (list2 != null) n.next = list2;
 
         return h.next;
+    }
 }
