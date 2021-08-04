9b57394fd6ea4cc3c4e3c85f824389dc33a185ad
cyyttaaioo <fxyclown@foxmail.com>
2021-08-04 22:37:48 +0800

    JZ34 第一个只出现一次的字符

---
+++ src/com/cyyttaaioo/nowcoder/sfo/JZ34.java
@@ -0,0 +1,13 @@
+package com.cyyttaaioo.nowcoder.sfo;
+
+public class JZ34 {
+    public int FirstNotRepeatingChar(String str) {
+        int[] counts = new int[58];
+        for(int i = 0; i < str.length(); i++)
+            counts[str.charAt(i)-'A'] +=1;
+        for(int i = 0; i < str.length(); i++)
+            if(counts[str.charAt(i)-'A'] == 1)
+                return i;
+        return -1;
+    }
+}
