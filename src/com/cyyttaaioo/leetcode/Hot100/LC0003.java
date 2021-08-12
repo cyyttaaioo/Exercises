b4b1d9feec3d49b0af7d47be62a799bdb3a69817
cyyttaaioo <fxyclown@foxmail.com>
2021-08-12 09:34:00 +0800

    3. 无重复字符的最长子串

---
+++ src/com/cyyttaaioo/leetcode/Hot100/LC0003.java
@@ -0,0 +1,22 @@
+package com.cyyttaaioo.leetcode.Hot100;
+
+import java.util.HashMap;
+
+public class LC0003 {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        int max = 0;
+        int left = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+
+                left = Math.max(left,map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+
+        return max;
+    }
+}
